a48859d2d15ddb025c48046b767005ba262bd0a0
YaoXiang <89034550@qq.com>
2020-07-02 20:41:46 +0800

    consumer异常demo

---
+++ whalefall-dubbo/whalefall-dubbo-consumer/src/main/java/org/whalefall/stack/dubbo/consumer/infrastructure/exception/ConsumerDubboException.java
@@ -0,0 +1,21 @@
+package org.whalefall.stack.dubbo.consumer.infrastructure.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.whalefall.stack.framework.infrastructure.exception.assertion.BizExceptionAssert;
+
+/**
+ * Copyright © 2020 Whale Fall All Rights Reserved
+ *
+ * @author YaoXiang
+ * @description 消费端异常
+ * @create 2020/7/2 7:29 下午
+ */
+@Getter
+@AllArgsConstructor
+public enum ConsumerDubboException implements BizExceptionAssert {
+    CONSUMER_DUBBO_EXCEPTION_TEST(1001, "dubbo消费端异常测试"),
+    ;
+    private int code;
+    private String message;
+}
