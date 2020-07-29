3205b2319a25c1d09d31dc2ea286e15c22dde9a9
YaoXiang <89034550@qq.com>
2020-07-29 15:47:39 +0800

    新增common组件

---
+++ whalefall-common/src/main/java/org/whalefall/stack/common/WhalefallCommonApplication.java
@@ -0,0 +1,13 @@
+package org.whalefall.stack.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhalefallCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhalefallCommonApplication.class, args);
+    }
+
+}
---
+++ whalefall-common/src/test/java/org/whalefall/stack/common/WhalefallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whalefall.stack.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhalefallCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
